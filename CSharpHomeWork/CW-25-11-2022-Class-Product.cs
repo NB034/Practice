f3ebfd726c7e50c96fa6c34ff75a9cff08b194f2
NB034 <104451273+NB034@users.noreply.github.com>
2022-12-16 21:18:34 +0500

    Organized into one folder

---
+++ CSharpHomeWork/CW-25-11-2022-Class-Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ClassWork
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public double Price { get; set; }
+    }
+}
